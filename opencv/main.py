eb2312238b4486c4c37dadccf3678f4304bb7ddf
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2023-01-08 22:25:53 +0700

    update

--- opencv/main.py
+++ opencv/main.py
@@ -8,26 +8,26 @@ from collections import Counter
 import cv2
 
 # Save Image
-def saveImage(image, file_name):
+def save_image(image, file_name):
 	print("Save successfully!")
 	cv2.imwrite(file_name, image)
 
 # Resize image
 @dispatch(np.ndarray, int)
-def resizeImage(image, percent):
+def resize_image(image, percent):
 	width = int(image.shape[1] * percent / 100)
 	height = int(image.shape[0] * percent / 100)
 	dimension = (width, height)
-	resized_image = cv2.resize(src = image,dsize = dimension,interpolation = cv2.INTER_CUBIC)
+	resized_image = cv2.resize(src = image, dsize = dimension,interpolation = cv2.INTER_CUBIC)
 	return resized_image
 
 @dispatch(np.ndarray, tuple)
-def resizeImage(image, dimension):
-	resized_image = cv2.resize(src = image,dsize = dimension,interpolation = cv2.INTER_CUBIC)
+def resize_image(image, dimension):
+	resized_image = cv2.resize(src = image, dsize = dimension,interpolation = cv2.INTER_CUBIC)
 	return resized_image
 
 #convert image color
-def convertColorImage(image, channel):
+def convert_color(image, channel):
 	if(channel == "GRAY"):
 		cvt_color_image = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)	
 	elif(channel == "HSV"):
@@ -42,37 +42,45 @@ def convertColorImage(image, channel):
 		cvt_color_image = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2Luv)
 	return cvt_color_image
 
-def displayImage(image, subplot, title):
-	cvt_color_image = convertColorImage(image, "RGB")
+def display_image(image, subplot, title):
+	cvt_color_image = convert_color(image, "RGB")
 	plt.subplot(subplot), plt.imshow(cvt_color_image), plt.title(title)
 	plt.xticks([]), plt.yticks([])
 
-def displayImages(images = [], subplots = [], titles = []):
+def display_images(images = [], subplots = [], titles = []):
 	for i in range(len(images)):
-		displayImage(images[i], subplots[i], titles[i])
+		display_image(images[i], subplots[i], titles[i])
 	plt.show()
 
-def displayColorImagesConversion(image):
-	gray_image = convertColorImage(image, "GRAY")
-	hsv_image = convertColorImage(image, "HSV")
+def display_color_images_conversion(image):
+	gray_image = convert_color(image, "GRAY")
+	hsv_image = convert_color(image, "HSV")
 	rgb_image = image
-	ycc_image = convertColorImage(image, "YCC")
-	hls_image = convertColorImage(image, "HLS")
-	luv_image = convertColorImage(image, "LUV")
+	ycc_image = convert_color(image, "YCC")
+	hls_image = convert_color(image, "HLS")
+	luv_image = convert_color(image, "LUV")
 
 	images = [gray_image, hsv_image, rgb_image, ycc_image, hls_image, luv_image]
 	subplots = [231,232,233, 234, 235, 236]
 	titles = ["GRAY", "HSV", "RGB", "YCC", "HLS", "LUV"]
-	displayImages(images, subplots, titles)
+	display_images(images, subplots, titles)
 
-def displayResizeImages(image, percent, dimension, title1, title2):
-	image_resized_p = resizeImage(image, percent)
-	image_resized_d = resizeImage(image, dimension)
+def display_resize_images(image, percent, dimension, title1, title2):
+	image_resized_p = resize_image(image, percent)
+	image_resized_d = resize_image(image, dimension)
 
 	cv2.imshow(title1, image_resized_p)
 	cv2.imshow(title2, image_resized_d)
 
-def displayHistogram(image, subplot, title):
+def display_negative_image(image):
+	dst = cv2.bitwise_not(image)
+	
+	images = [image, dst]
+	subplots = [121, 122]
+	titles = ["Original Image", "Negative Image"]
+	display_images(images, subplots, titles)
+
+def display_histogram(image, subplot, title):
 	hist,bins = np.histogram(image.flatten(),256,[0,256])
 	cdf = hist.cumsum()
 	cdf_normalized = cdf * float(hist.max()) / cdf.max()
@@ -83,87 +91,207 @@ def displayHistogram(image, subplot, title):
 	plt.xlim([0,256])
 	plt.legend(('cdf','histogram'), loc = 'upper left')
 
-def displayHistograms(images = [], subplots = [], titles = []):
+def display_histograms(images = [], subplots = [], titles = []):
 	for i in range(len(images)):
-		displayHistogram(images[i], subplots[i], titles[i])
+		display_histogram(images[i], subplots[i], titles[i])
 	plt.show()
 
-def displayEqualizedHistogram(image):
-	image = convertColorImage(image, "GRAY")
+def display_equalized_histogram(image):
+	image = convert_color(image, "GRAY")
 	dst = cv2.equalizeHist(image)
 	
 	images = [image, dst]
 	subplots = [121, 122]
 	titles = ["Original Image Histogram", "Equalized Image Histogram"]
-	displayHistograms(images, subplots, titles)
+	display_histograms(images, subplots, titles)
 
-def displayHistogramMatching(image):
+def display_histogram_matching(image):
 	colors = ('b','g','r')
 	for i,color in enumerate(colors):
 		histr = cv2.calcHist([image],[i],None,[256],[0,256])
 		plt.plot(histr,color = color)
 		plt.xlim([0,256])
-	plt.show()
+	# plt.show()
 
-def lowPassFilter(image, ksize):
+# def displayConcatImage(image1, image2):
+# 	image = cv2.hconcat(image1, image2)
+# 	cv2.imshow("Concat image", image)
+
+def enhance_linear_contrast(image, alpha, beta):
+	dst_image = np.zeros(image.shape, image.dtype)
+
+	for h in range(image.shape[0]):
+		for w in range(image.shape[1]):
+			for c in range(image.shape[2]):
+				dst_image[h,w,c] = np.clip(alpha*image[h,w,c] + beta, 0, 255)
+
+	images = [image, dst_image]
+	subplots = [121, 122]
+	titles = ["Original Image" ,"Linear Constrast Image"]
+	display_images(images, subplots, titles)
+
+def enhance_gamma_power_law(image, c, gamma):
+	img_float32 = np.float32(image)
+	dst_image = np.array(c*(img_float32/255)**gamma)
+	
+	images = [image, dst_image]
+	subplots = [121, 122]
+	titles = ["Original Image" ,"Gamma Power Law Image"]
+	display_images(images, subplots, titles)
+
+
+def enhance_piecewise_linear(image, x1, x2, k1, k2, y1):
+	img_float32 = np.float32(image)
+	dst = img_float32
+	condlist = [(dst < x1), dst >= x2]
+	funclist = [lambda dst:k1*dst + y1 - k1*x1, lambda dst:k2*dst + y1 + k2*x2]
+	dst = np.piecewise(dst, condlist, funclist)
+
+	images = [image, dst]
+	subplots = [121, 122]
+	titles = ["Original Image" ,"Piecewise Linear Image"]
+	display_images(images, subplots, titles)
+
+
+def enhance_non_linear_contrast(image, threshold, grid_size):
+	gray = convert_color(image, "GRAY")
+
+	clahe = cv2.createCLAHE(clipLimit = threshold, tileGridSize= grid_size)
+	equalize = clahe.apply(gray)
+
+	images = [gray, equalize]
+	subplots = [121, 122]
+	titles = ["Gray image", "Adaptive histogram equalization"]
+	display_images(images, subplots, titles)
+
+def low_pass_filter(image, ksize):
 	dst_blur = cv2.blur(src = image, ksize = ksize)
 	dst_gaussian = cv2.GaussianBlur(src = image, ksize = ksize, sigmaX = 0, sigmaY = 0)
 
 	images = [image, dst_blur, dst_gaussian]
 	subplots = [131, 132, 133]
 	titles = ["Original Image" ,"Median filter", "Gaussian filter"]
-	displayImages(images, subplots, titles)
+	display_images(images, subplots, titles)
 
-def medianFilter(image, ksize):
+def median_filter(image, ksize):
 	dst_median = cv2.medianBlur(src = image, ksize = ksize)
 
 	images = [image, dst_median]
 	subplots = [121, 122]
 	titles = ["Original Image" ,"Median Filter Image"]
-	displayImages(images, subplots, titles)
+	display_images(images, subplots, titles)
 
-def highPassFilter(image, kernel):
+def high_pass_filter(image, kernel):
 	dst_sharpen=cv2.filter2D(src = image,ddepth = -1,kernel = kernel)
 
 	images = [image, dst_sharpen]
 	subplots = [121, 122]
 	titles = ["Original Image" ,"Sharpened Filter Image"]
-	displayImages(images, subplots, titles)
+	display_images(images, subplots, titles)
 
-def filterMinAndMax(image, kernel):
+def filter_min_max(image, kernel):
 	dst_erosion = cv2.erode(src = image, kernel = kernel, iterations=1)
 	dst_dilation = cv2.dilate(src = image, kernel = kernel, iterations=1)
 
 	images = [image, dst_erosion, dst_dilation]
 	subplots = [131, 132, 133]
 	titles = ["Original Image" ,"Min Filter Image", "Max Filter Image"]
-	displayImages(images, subplots, titles)
+	display_images(images, subplots, titles)
 
-def filterCloseAndOpen(image, kernel):
+def filter_close_open(image, kernel):
 	dst_opening = cv2.morphologyEx(src = image, op = cv2.MORPH_OPEN, kernel = kernel)
 	dst_closing = cv2.morphologyEx(src = image, op = cv2.MORPH_CLOSE, kernel = kernel)
 
 	images = [image, dst_opening, dst_closing]
 	subplots = [131, 132, 133]
 	titles = ["Original Image" ,"Opening Filter Image", "Closing Filter Image"]
-	displayImages(images, subplots, titles)
+	display_images(images, subplots, titles)
+
+def display_affine_transform(image, pts1, pts2):
+	rows, cols, ch = image.shape
+	M = cv2.getAffineTransform(pts1, pts2)
+	dst = cv2.warpAffine(image, M, (cols, rows))
+
+	images = [image, dst]
+	subplots = [121, 122]
+	titles = ["Original Image" ,"Affine Transform Image"]
+	display_images(images, subplots, titles)
+
+def display_perspective_transform(image, pts1, pts2):
+	rows, cols, ch = image.shape
+	M = cv2.getPerspectiveTransform(pts1, pts2)
+	dst = cv2.warpPerspective(image, M, (cols, rows))
+	
+	images = [image, dst]
+	subplots = [121, 122]
+	titles = ["Original Image" ,"Perspective Transform Image"]
+	display_images(images, subplots, titles)
+
+def display_adaptive_thresholding(image):
+	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	
+	ret, th1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+	th2 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11,2)
+	th3 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,2)
+
+	titles = ['Original Image', 'Global Thresholding (v = 127)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+	images = [image, th1, th2, th3]
+	subplots = [221,222,223,224]
+	display_images(images, subplots, titles)
+
+def display_thresholding(image):
+	ret, thresh1 = cv2.threshold(image, 127,255, cv2.THRESH_BINARY)
+	ret, thresh2 = cv2.threshold(image, 127,255, cv2.THRESH_BINARY_INV)
+	ret, thresh3 = cv2.threshold(image, 127,255, cv2.THRESH_TRUNC)
+	ret, thresh4 = cv2.threshold(image, 127,255, cv2.THRESH_TOZERO)
+	ret, thresh5 = cv2.threshold(image, 127,255, cv2.THRESH_TOZERO_INV)
+
+	titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+	images = [image, thresh1, thresh2, thresh3, thresh4, thresh5]
+	subplots = [231, 232, 233, 234, 235, 236]
+
+	display_images(images, subplots, titles)
+
 
 def main():
+
 	image = cv2.imread("car.jpg")
+
+	# display_color_images_conversion(image)
+	# display_resize_images(image, 50, (200,200), "Scale 50%", "200x200")
+	# display_negative_image(image)
+	# display_equalized_histogram(image)
+	# display_histogram_matching(image)
 	
-	# displayColorImagesConversion(image)
-	# displayResizeImages(image, 50, (200,200), "Scale 50%", "200x200")
-	# displayEqualizedHistogram(image)
-	# displayHistogramMatching(image)
-	# lowPassFilter(image, (5,7))
-	# medianFilter(image, 7)
+	# enhance_linear_contrast(image, 2, -5)
+	# enhance_non_linear_contrast(image, 40.0, (8,8))
+	# enhance_gamma_power_law(image, 100, 2.0)
+	# enhance_piecewise_linear(image, 20, 30, 50, 60, 70)
+
+	# low_pass_filter(image, (5,7))
+	# median_filter(image, 7)
 
 	# kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-	# highPassFilter(image, kernel)
+	# high_pass_filter(image, kernel)
 
-	kernel = np.ones((5,5))
-	# filterMinAndMax(image, kernel)
-	filterCloseAndOpen(image, kernel)
+	# kernel = np.ones((5,5))
+	# filter_min_max(image, kernel)
+	# filter_close_open(image, kernel)
+
+	# pts1 = np.float32([[50,50], [200,50], [50,200]])
+	# pts2 = np.float32([[10,100], [200,50], [100,250]])
+	# display_affine_transform(image, pts1, pts2)
+
+	# pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
+	# pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
+	# display_perspective_transform(image, pts1, pts2)
+
+	# display_thresholding(image)
+	# display_adaptive_thresholding(image)
+
+	x7_image = cv2.imread("x7_image.png")
+	enhance_gamma_power_law(x7_image, 0.1, 1.0)
+	display_histogram(x7_image, "x7_image", "title")
 main()
 
 # key end cv2 running
@@ -199,124 +327,44 @@ def blur(image):
 
 #####################################################################
 # image translation
+# image = cv2.imread('car.jpg')
+# rows, cols, channels = image.shape
 # M = np.float32([[1,0,100], [0, 1, 50]]) 
 # 100 is width translation, 50 is height translation
-# _image_translation = cv2.warpAffine(image, M, (cols, rows))
+# dst = cv2.warpAffine(image, M, (cols, rows))
 
 # cv2.imshow('img', dst)
-# cv2.imwrite('image translation.jpg', _image_translation)
 
-#####################################################################
-# Affine transformation
-# rows, cols, ch = image.shape
-
-# pts1 = np.float32([[50,50], [200,50], [50,200]])
-# pts2 = np.float32([[10,100], [200,50], [100,250]])
-
-# M = cv2.getAffineTransform(pts1, pts2)
-
-# image_affine = cv2.warpAffine(image, M, (cols, rows))
-
-#####################################################################
-# Perspective Transform
-# pts1 = np.float32([[56,65],[368,52],[28,387],[389,390]])
-# pts2 = np.float32([[0,0],[300,0],[0,300],[300,300]])
-
-# M = cv2.getPerspectiveTransform(pts1, pts2)
-# image_perspective = cv2.warpPerspective(image, M, (300, 300))
-
-# plt.subplot(121), plt.imshow(image), plt.title('Input')
-# plt.subplot(122), plt.imshow(image_affine), plt.title('Output')
-# plt.show()
-
-######################################################################
-# thresholding 
-# ret, thresh1 = cv2.threshold(image, 127,255, cv2.THRESH_BINARY)
-# ret, thresh2 = cv2.threshold(image, 127,255, cv2.THRESH_BINARY_INV)
-# ret, thresh3 = cv2.threshold(image, 127,255, cv2.THRESH_TRUNC)
-# ret, thresh4 = cv2.threshold(image, 127,255, cv2.THRESH_TOZERO)
-# ret, thresh5 = cv2.threshold(image, 127,255, cv2.THRESH_TOZERO_INV)
-
-# titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
-# images = [image, thresh1, thresh2, thresh3, thresh4, thresh5]
-
-# for i in range(6):
-# 	plt.subplot(2,3,i+1), plt.imshow(images[i],'gray', vmin = 0, vmax = 255)
-# 	plt.title(titles[i])
-# 	plt.xticks([]),plt.yticks([])
-
-# plt.show()
-
-#####################################################################
-# Adaptive Thresholding
-# image = cv2.imread('20_percent_image.jpg')
-# image = cv2.medianBlur(image, 5)
-
-# gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# ret, th1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
-
-# th2 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11,2)
-# th3 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,2)
-
-# titles = ['Original Image', 'Global Thresholding (v = 127)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-# images = [image, th1, th2, th3]
-
-# cv2.imshow('image', image)
-
-# for i in range(4):
-# 	plt.subplot(2,2,i+1), plt.imshow(images[i],'gray')
-# 	plt.title(titles[i])
-# 	plt.xticks([]), plt.yticks([])
-
-# plt.show()
-
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+S
 #####################################################################
 # Otsu's Binarization
 # image = cv2.imread('index.jpg')
 
-# gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# global thresholding
-# ret1,th1 = cv2.threshold(gray_image,127,255,cv2.THRESH_BINARY)
+# def Otsu_binarization(image):
+# 	img_float32 = np.float32(image)
+# 	gray_image = cv2.cvtColor(img_float32, cv2.COLOR_BGR2GRAY)
 
-# Otsu's thresholding 
-# ret2,th2 = cv2.threshold(gray_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# 	# global thresholding
+# 	ret1,th1 = cv2.threshold(gray_image,127,255,cv2.THRESH_BINARY)
 
-#Otsu's thresholding after Gaussian filtering
-# blur = cv2.GaussianBlur(gray_image, (5,5) , 0)
-# ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# 	# Otsu's thresholding 
+# 	ret2,th2 = cv2.threshold(gray_image,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-# images = [image, 0, th1, image, 0, th2, blur, 0, th3]
-# titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v = 127)',
-# 'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
-# 'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
+# 	#Otsu's thresholding after Gaussian filtering
+# 	blur = cv2.GaussianBlur(gray_image, (5,5) , 0)
+# 	ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-# for i in range(3):
-# 	plt.subplot(3,3,i*3+1), plt.imshow(images[i*3], 'gray')
-# 	plt.title(titles[i*3]), plt.xticks([]), plt.yticks([])
-# 	plt.subplot(3,3,i*3+2),plt.hist(images[i*3].ravel(),256)
-# 	plt.title(titles[i*3+1]), plt.xticks([]), plt.yticks([])
-# 	plt.subplot(3,3,i*3+3),plt.imshow(images[i*3+2],'gray')
-# 	plt.title(titles[i*3+2]), plt.xticks([]), plt.yticks([])
-# plt.show()
+# 	images = [image, 0, th1, image, 0, th2, blur, 0, th3]
+# 	titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v = 127)',
+# 	'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
+# 	'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
+# 	subplots = [331, 332, 333, 334, 335, 336, 337, 338, 339]
+# 	display_images(images, subplots, titles)
 
-#####################################################################
-# avaraging
-# image = cv2.imread('blur_image.jfif')
-
-# scale_percent = 50
-# width =int(image.shape[0] * scale_percent / 100) 
-# height = int(image.shape[1] * scale_percent/ 100)
-# dimension = (width, height)
-# percent_image = cv2.resize(image, dimension, cv2.INTER_CUBIC)
-# kernel = np.ones((5,5), np.float32)/25
-# dst = cv2.filter2D(percent_image, -1, kernel)
-
-# plt.subplot(121), plt.imshow(percent_image), plt.title('Original')
-# plt.xticks([]), plt.yticks([])
-# plt.subplot(122), plt.imshow(dst), plt.title('Averaging')
-# plt.xticks([]), plt.yticks([])
-# plt.show()
+# image = cv2.imread('car.jpg')
+# Otsu_binarization(image)
 
 def visualizeRGB(image, title):
 	r, g, b = cv2.split(image)
